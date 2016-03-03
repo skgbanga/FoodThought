9a8dbb6f72ba09882c23b7ee3cb498f6cce4e37c
skgbanga <sandeep.skg3@gmail.com>
2016-03-03 10:48:31 -0500

    Move Data to namespace and cpp file, and add comments

--- server/Server.cpp
+++ server/Server.cpp
@@ -2,63 +2,70 @@
 
 namespace Server
 {
-
-Data data {};
-
-// definitons of all functions
-void Initialize()
-{
-   // create lib event base config
-   data.m_upEventConfig.reset(event_config_new());
-   CHECK_NOTNULL(data.m_upEventConfig.get());
-   auto flag = event_config_set_flag(data.m_upEventConfig.get(), EVENT_BASE_FLAG_NOLOCK);
-   CHECK_EQ(flag, 0) << "Couldn't set nolock flag!";
-   // More flags can be set in the config. Refer to
-   // http://www.wangafu.net/~nickm/libevent-book/Ref2_eventbase.html
-
-   // create lib event base
-   data.m_upEventBase.reset(event_base_new_with_config(data.m_upEventConfig.get()));
-   CHECK_NOTNULL(data.m_upEventBase.get());
-
-   // create timer event
-   data.m_upTimerEvent.reset(event_new(data.m_upEventBase.get(), -1, EV_TIMEOUT | EV_PERSIST, onTimeout, nullptr));
-   CHECK_NOTNULL(data.m_upTimerEvent.get());
-   timeval temp = {data.m_timeout, 0};
-   CHECK_EQ(evtimer_add(data.m_upTimerEvent.get(), &temp), 0) << "Couldn't add timer event!";
-
-   // small utility checking lambda
-   auto checkError = [](int err, const char* act)
+   namespace Data
    {
-      auto errCode = EVUTIL_SOCKET_ERROR();
-      CHECK_GE(err, 0) << "Error while " << act
-                       << ". Reason :" << evutil_socket_error_to_string(errCode);
-   };
+      int               m_timeout {5};
+      short             m_port {8080};
+      int               m_maxNoClients {128};
+      evutil_socket_t   m_serverSocketFd {0};
+      std::unique_ptr<event_config, decltype(&event_config_free)>  m_upEventConfig {nullptr, event_config_free};
+      std::unique_ptr<event_base, decltype(&event_base_free)>      m_upEventBase {nullptr, event_base_free};
+      std::unique_ptr<event, decltype(&event_free)>                m_upTimerEvent {nullptr, event_free};
+   }
+   using namespace Data;
 
-   // Create a non-blocking, re-useable server socket
-   data.m_serverSocketFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-   checkError(data.m_serverSocketFd, "creation");
-   evutil_make_socket_nonblocking(data.m_serverSocketFd);
-   evutil_make_listen_socket_reuseable(data.m_serverSocketFd);
+   // definitons of all functions
+   void Initialize()
+   {
+      // create lib event base config
+      m_upEventConfig.reset(event_config_new());
+      CHECK_NOTNULL(m_upEventConfig.get());
+      auto flag = event_config_set_flag(m_upEventConfig.get(), EVENT_BASE_FLAG_NOLOCK);
+      CHECK_EQ(flag, 0) << "Couldn't set nolock flag!";
 
-   // Bind it to an address
-   sockaddr_in serverAddress {AF_INET, htonl(INADDR_ANY), htons(data.m_port), {0}};
-   auto errBind = bind(data.m_serverSocketFd, (sockaddr *)&serverAddress, sizeof(serverAddress));
-   checkError(errBind, "binding");
+      // create lib event base
+      m_upEventBase.reset(event_base_new_with_config(m_upEventConfig.get()));
+      CHECK_NOTNULL(m_upEventBase.get());
 
-   // start listening
-   auto errListen = listen(data.m_serverSocketFd, data.m_maxNoClients);
-   checkError(errListen, "listening");
-}
+      // create timer event
+      m_upTimerEvent.reset(event_new(m_upEventBase.get(), -1, EV_TIMEOUT | EV_PERSIST, onTimeout, nullptr));
+      CHECK_NOTNULL(m_upTimerEvent.get());
+      timeval temp = {m_timeout, 0};
+      CHECK_EQ(evtimer_add(m_upTimerEvent.get(), &temp), 0) << "Couldn't add timer event!";
 
-void onTimeout(evutil_socket_t, short, void*)
-{
-   // One could call event_base_gettimeofday_cached to get current cached time in this callback
-   LOG(INFO) << "Called after " << data.m_timeout << " seconds";
-}
+      // small utility checking lambda
+      auto checkError = [](int err, const char* act)
+      {
+         auto errCode = EVUTIL_SOCKET_ERROR();
+         CHECK_GE(err, 0) << "Error while " << act
+                          << ". Reason :" << evutil_socket_error_to_string(errCode);
+      };
 
-void run()
-{
-   event_base_dispatch(data.m_upEventBase.get());
-}
+      // Create a non-blocking, re-useable server socket
+      m_serverSocketFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+      checkError(m_serverSocketFd, "creation");
+      evutil_make_socket_nonblocking(m_serverSocketFd);
+      evutil_make_listen_socket_reuseable(m_serverSocketFd);
+
+      // Bind it to an address
+      sockaddr_in serverAddress {AF_INET, htonl(INADDR_ANY), htons(m_port), {0}};
+      auto errBind = bind(m_serverSocketFd, (sockaddr *)&serverAddress, sizeof(serverAddress));
+      checkError(errBind, "binding");
+
+      // start listening
+      auto errListen = listen(m_serverSocketFd, m_maxNoClients);
+      checkError(errListen, "listening");
+   }
+
+   void onTimeout(evutil_socket_t, short, void*)
+   {
+      // One could call event_base_gettimeofday_cached to get current cached time in this callback
+      LOG(INFO) << "Called after " << m_timeout << " seconds";
+   }
+
+   void run()
+   {
+      event_base_dispatch(m_upEventBase.get());
+   }
 
 } // end namespace server
--- server/Server.h
+++ server/Server.h
@@ -4,20 +4,16 @@
 #include <glog/logging.h>
 #include <event2/event.h>
 
+// Libevent refrence manual: http://www.wangafu.net/~nickm/libevent-book/TOC.html
+// Most libevent functions take callback functions as arguments, and since non-static
+// memeber functions can't be passed, this makes writing Server as a class useless.
+// (A single monolithic static server class? No)
+// Hence, defining Server as a namespace containing non-member functions which can be used
+// as callback functions in libevent API
+
 namespace Server
 {
    void Initialize();
    void onTimeout(evutil_socket_t, short, void*);
    void run();
-
-   struct Data
-   {
-      int               m_timeout {5};
-      short             m_port {8080};
-      int               m_maxNoClients {128};
-      evutil_socket_t   m_serverSocketFd {0};
-      std::unique_ptr<event_config, decltype(&event_config_free)>  m_upEventConfig {nullptr, event_config_free};
-      std::unique_ptr<event_base, decltype(&event_base_free)>      m_upEventBase {nullptr, event_base_free};
-      std::unique_ptr<event, decltype(&event_free)>                m_upTimerEvent {nullptr, event_free};
-   };
 }
