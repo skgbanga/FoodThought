24143f6620d474f16d8f0cc5ae35d29438d09c7f
skgbanga <sandeep.skg3@gmail.com>
2016-03-03 08:57:02 -0500

    remove LibEventDeleters, and pass pointer to function as custom deleter

--- server/Server.h
+++ server/Server.h
@@ -4,8 +4,6 @@
 #include <glog/logging.h>
 #include <event2/event.h>
 
-#include "LibEventDeleters.h"
-
 namespace Server
 {
    void Initialize();
@@ -18,8 +16,8 @@ namespace Server
       short             m_port {8080};
       int               m_maxNoClients {128};
       evutil_socket_t   m_serverSocketFd {0};
-      std::unique_ptr<event_config, Deleters::EventConfig>  m_upEventConfig {nullptr};
-      std::unique_ptr<event_base, Deleters::EventBase>      m_upEventBase {nullptr};
-      std::unique_ptr<event, Deleters::Event>               m_upTimerEvent {nullptr};
+      std::unique_ptr<event_config, decltype(&event_config_free)>  m_upEventConfig {nullptr, event_config_free};
+      std::unique_ptr<event_base, decltype(&event_base_free)>      m_upEventBase {nullptr, event_base_free};
+      std::unique_ptr<event, decltype(&event_free)>                m_upTimerEvent {nullptr, event_free};
    };
 }
