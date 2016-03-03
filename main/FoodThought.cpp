4abe9d0d25ddda34acd2fb9fb511221c8f1f8ced
skgbanga <sandeep.skg3@gmail.com>
2016-03-02 23:30:45 -0500

    change logging file name to Logging

--- main/FoodThought.cpp
+++ main/FoodThought.cpp
@@ -1,5 +1,5 @@
 #include "server/DefaultTypeDict.h"
-#include "utils/logging.h"
+#include "utils/Logging.h"
 #include "server/Server.h"
 
 // Use your own type dict here (which might contain a different strategy)
