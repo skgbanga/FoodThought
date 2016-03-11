8331613861b457e5a3e4d824a3385696d3889e94
skgbanga <sandeep.skg3@gmail.com>
2016-03-10 23:30:45 -0500

    configure null logging

--- strategy/main.t.cc
+++ strategy/main.t.cc
@@ -5,7 +5,7 @@
 int main(int argc, char* argv[])
 {
    // switch INFO logging off
-   //ConfigureNullLogging(argv[0]);
+   ConfigureNullLogging(argv[0]);
 
    testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
