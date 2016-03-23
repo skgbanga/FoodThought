c54fdbe83d65b7c635ea77c2ae8cb97161c3d719
skgbanga <sandeep.skg3@gmail.com>
2016-03-23 08:54:38 -0400

    add support for env variables in config objects

--- utils/ConfigObject.h
+++ utils/ConfigObject.h
@@ -40,13 +40,25 @@ class ConfigObject
          std::regex r2("(.*)\\$\\((.*)\\)(.*)"); // regex for things like abc$(def)ghi
          std::smatch m2;
          auto result2 = std::regex_match(val1, m2, r2);
-         if (not result2) // if it doesn't match, then we are good
+         if (!result2) // if it doesn't match, then we are good
+         {
             return val1;
+         }
+         else
+         {
+            auto token = m2[2].str();
+            std::string tokenValue = "";
 
-         auto innerVar = section + "." + m2[2].str();
-         auto val2 = getStringNoLog(innerVar, def);
-         auto resultingVal = m2[1].str() + val2 + m2[3].str();
-         return resultingVal;
+            // check if the token is an env variable
+            if (auto* charPtr = std::getenv(token.c_str())) // check to see if env variable
+               tokenValue =  std::string(charPtr);
+            else
+               tokenValue = getStringNoLog(section + "." + token, def);
+
+            // otherwise
+            auto resultingVal = m2[1].str() + tokenValue + m2[3].str();
+            return resultingVal;
+         }
       }
 
       std::string getString(const std::string& path, std::string def) const
--- utils/ConfigObject.t.cc
+++ utils/ConfigObject.t.cc
@@ -1,16 +1,27 @@
 #include <gtest/gtest.h>
 #include "ConfigObject.h"
 #include <string>
+#include <stdlib.h>
 
 
 TEST(ConfigObjectTest, readDollarVar)
 {
    ConfigObject config("test.ini");
-   std::string val1 = config.getString("A.var1", "");
-   std::string val2 = config.getString("A.var2", "");
-   std::string val3 = config.getString("A.var3", "");
+   auto val1 = config.getString("A.var1", "");
+   auto val2 = config.getString("A.var2", "");
+   auto val3 = config.getString("A.var3", "");
 
    EXPECT_STREQ("Jerry", val1.c_str());
    EXPECT_STREQ("TomAndJerry", val2.c_str());
    EXPECT_STREQ("TomAndJerryAndSpike", val3.c_str());
+}
+
+TEST(ConfigObject, readEnvVar)
+{
+   // set the env variable
+   ASSERT_TRUE(!setenv("PUP", "Tyke", true));
+   ConfigObject config("test.ini");
+   auto val1 = config.getString("B.var1", "");
+
+   EXPECT_STREQ("SpikeLovesTyke", val1.c_str());
 }
